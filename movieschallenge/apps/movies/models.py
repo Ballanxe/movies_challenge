3432e7d72d9f2e4757f7ba411366eb744dd4d721
Alberto <rinconegocio@gmail.com>
2019-12-17 22:26:48 -0300

    refactoring authentication and person model

--- movieschallenge/apps/movies/models.py
+++ movieschallenge/apps/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from movieschallenge.apps.core.models import TimestampedModel
+
+
+class Movie(TimestampedModel):
+
+    title = models.CharField('Title',db_index=True, max_length=255)
+    release_year = models.DateField('Release year')
+    casting = models.ManyToManyField('app.Persons', related_name='casting')
+    directors = models.ManyToManyField('app.Persons', related_name='directors')
+    producers = models.ManyToManyField('app.Persons', related_name='producers')
+
+	def __str__(self):
+		return self.title.capitalize()
--- movieschallenge/apps/persons/models.py
+++ movieschallenge/apps/persons/models.py
@@ -1,93 +1,30 @@
-from datetime import datetime, timedelta
-
-import jwt
-
-from django.conf import settings
-from django.contrib.auth.models import (
-	AbstractBaseUser, 
-	BaseUserManager, 
-	PermissionsMixin	
-)
 from django.db import models
 
 from movieschallenge.apps.core.models import TimestampedModel
 
 
-class PersonManager(BaseUserManager):
+class Person(TimestampedModel):
+    """Person"""
+    
+    first_name = models.CharField(db_index=True, max_length=255, unique=True)
+    last_name = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    aliases = models.CharField(db_index=True, max_length=255, unique=True)
+    movies_as_actor = models.ForeignKey('apps.Movies', related_name='actors', on_delete=models.CASCADE)
+    movies_as_director = models.ForeignKey(
+        'apps.Movies', related_name='directors', on_delete=models.CASCADE)
+    movies_as_producer = models.ForeignKey(
+        'apps.Movies', related_name='producers', on_delete=models.CASCADE)
 
-	def create_user(self, username, email, password=None):
-		"""Create and return a `User` with an email, username and password."""
-		if username is None:
+    def __str__(self):
 
-			raise TypeError('Users must have a username.')
-		if email is None:
-
-			raise TypeError('Users must have an email adress')
-
-		user = self.model(username=username, email=self.normalize_email(email))
-		user.set_password(password)
-		user.save()
-		return user
-
-	def create_superuser(self, username, email, password):
-		"""
-		Create and return a `User` with superuser (admin) permissions.
-		"""
-		if password is None:
-			raise TypeError('Superusers must have a password')
-
-		user = self.create_user(username, email, password)
-		user.is_superuser = True
-		user.is_staff = True
-		user.save()
-
-		return user 
-
-class Person(AbstractBaseUser, PermissionsMixin, TimestampedModel):
+        return self.email
 
 
-	username = models.CharField(db_index=True, max_length=255, unique=True)
-	email = models.EmailField(db_index=True, unique=True)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    def get_full_name(self):
 
-	class Meta:
+        return f'{self.first_name} {self.last_name}'
 
-	    verbose_name = "Reviewer"
-	    verbose_name_plural = "Reviewers"
+    def get_aliases(self):
 
-
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
-
-	objects = PersonManager()
-
-	def __str__(self):
-
-		return self.email
-
-	@property
-	def token(self):
-
-		return self._generate_jwt_token()
-
-	def get_full_name(self):
-
-		return self.username
-
-	def get_short_name(self):
-
-		return self.username
-
-	def _generate_jwt_token(self):
-
-		dt = datetime.now() + timedelta(days=60)
-
-		token = jwt.encode({
-
-			'id':self.pk,
-			'exp': int(dt.strftime('%s'))
-
-		}, settings.SECRET_KEY, algorithm='HS256')
-
-		return token.decode('utf-8')
+        return self.aliases
